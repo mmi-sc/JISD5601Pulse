c9a49861faf24607ac9adf6e98fe89fd4367b241
Yasuhiro Magara <gama@users.sourceforge.jp>
2015-05-17 00:08:02 +0900

    Create from minimum source.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
 ===============================================================================
- Name        : JISD5601.c
+ Name        : JISD5601Pulse.c
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
@@ -8,12 +8,8 @@
 ===============================================================================
 */
 
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
-#include "chip.h"
-#else
-#include "board.h"
-#endif
+#ifdef __USE_CMSIS
+#include "LPC8xx.h"
 #endif
 
 #include <cr_section_macros.h>
@@ -23,18 +19,6 @@
 // TODO: insert other definitions and declarations here
 
 int main(void) {
-
-#if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
 
     // TODO: insert code here
 
--- src/sct_user.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SCT_USER_H__
-#define __SCT_USER_H__
-#include "chip.h"
-
-#define delay (6623)
-#endif

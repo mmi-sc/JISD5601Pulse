395f4bd4fd247f6ff75cc358dcb9bef509d2d919
Yasuhiro Magara <gama@users.sourceforge.jp>
2015-05-17 10:39:02 +0900

    Blink a LED slowly by SCT.

--- src/main.c
+++ src/main.c
@@ -13,14 +13,39 @@
 #endif
 
 #include <cr_section_macros.h>
+#include "sct_fsm.h"
 
-// TODO: insert other include files here
+void SwitchMatrix_Init();
 
-// TODO: insert other definitions and declarations here
+#define S1A (0x3E)  // LCD address
+#define S1W (S1A<<1)
+
+/*
+static uint8_t initSeq[11] = { 10,
+		S1W, 0x38,0x39, 0x14, 0x72,
+		0x57, 0x6A, 0x38, 0x0C, 0x01 };
+static uint8_t string1[4][10] = {
+		{ 3, S1W, 0x06 , 0x80 },
+		{ 8, S1W, 0x40, 0xCA, 0xDB, 0xB0, 0x20,0x20,0x20 },
+		{ 3, S1W, 0x06, 0xC0 },
+		{ 8, S1W, 0x40, 'W' , 'o', 'r', 'l', 'd' , '.'}
+};
+
+static uint8_t string2[4][10] = {
+		{ 3, S1W, 0x06 , 0xC0 },
+		{ 8, S1W, 0x40, 'H', 'e' ,'l', 'l', 'o','.'},
+		{ 3, S1W, 0x06, 0x80 },
+		{ 8, S1W, 0x40, 'W' , 'o', 'r', 'l', 'd' , '.'}
+};
+*/
 
 int main(void) {
+	SystemCoreClockUpdate();
+    SwitchMatrix_Init();
 
-    // TODO: insert code here
+    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 8);
+    sct_fsm_init();
+    LPC_SCT->CTRL_U &= ~(1<<2);
 
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
---
+++ src/sct_user.h
@@ -0,0 +1,6 @@
+#ifndef __SCT_USER_H__
+#define __SCT_USER_H__
+#include "lpc8xx.h"
+
+#define counter_limit (8477237)
+#endif
